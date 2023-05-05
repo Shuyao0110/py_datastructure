8effabd00d6a1d7acb4dee752b4e250572fbdb6e
Shuyao0110 <113600677+Shuyao0110@users.noreply.github.com>
2023-05-05 11:03:20 +0800

    Update sortColors.py

--- sortColors.py
+++ sortColors.py
@@ -26,3 +26,34 @@ class Solution(object):
                 left += 1
                 right -= 1
         return nums
+    
+    # 三根指针扫描，index，left，right
+    # 小于1，给left；大于1，给right
+    def sortColors(self, nums):
+        left, index, right = 0, 0, len(nums)-1
+        while index <= right:
+            if nums[index] == 0:
+                nums[left], nums[index] = nums[index], nums[left]
+                index += 1
+                left += 1
+            elif nums[index] == 2:
+                nums[index], nums[right] = nums[right], nums[index]
+                index += 1
+                right -= 1
+            else:
+                index += 1
+
+    # count sort放入字典然后再写出来
+    def sortColor(self, nums):
+        colors = {0: 0, 1: 0, 2: 0}
+        for index in nums:
+            colors[index] += 1
+        result = []
+        self.print_color(0, colors[0], result)
+        self.print_color(1, colors[1], result)
+        self.print_color(2, colors[2], result)
+
+    def print_color(self, num1, num2, result):
+        for i in range(num2+1):
+            result.append(num1)
+
